5d26001de7bcafdf849ba9d27dbe84bb51c0af5b
VACHARA VIRIYAKUL <63010871@KMITL.AC.TH>
2020-08-11 17:27:34 +0700

    Full

--- Challenge_Loop/Challenge.cpp
+++ Challenge_Loop/Challenge.cpp
@@ -1,29 +1,89 @@
 #include<stdio.h>
+#include<math.h>
+double avg(int a ,int b);
+double SD(int a, int b);
+
 int main()
 {
-	int a ,b;
-	printf("input : ");
-	scanf("%d %d", &a, &b);
+	int a, b;
+	scanf("%d %d" ,&a ,&b);
 	if (a > b)
 	{
-		for (int i = b; i <= a; i++)
+		for (int i=a ; i >=b ; i--)
 		{
-			printf("%d ", i);
+			printf("%d " ,i);
 		}
+		printf("\n");
+	}
+	else if (a < b)
+	{
+		for (int j=a ; j<=b ; j++)
+		{
+			printf("%d " ,j);
+		}
+		printf("\n");
+	}
+	else if (a = b)
+	{
+		printf("%d\n" ,a);
+	}
+	else
+		printf("Error\n");
+
+	printf("Average = %.1lf\n" ,avg(a, b));
+	printf("SD = %.2lf\n" ,SD(a,b));
+	return 0;
+}
+
+double avg(int a, int b) {
+	double sum = 0;
+	double avg;
+	double k = 0;
+	if (a > b)
+	{
+		for (int i = a; i >= b; i--)
+		{
+			sum += i;
+			k++;
+		}
+		avg = sum / k;
+		return avg;
 	}
 	else if (a < b)
 	{
 		for (int j = a; j <= b; j++)
 		{
-			printf("%d ", j);
+			sum += j;
+			k++;
 		}
+		avg = sum / k;
+		return avg;
 	}
-	else if (a = b)
-	{
-		printf("%d", a);
-	}
-	else
-		printf("Error");
+}
 
-	return 0;
+double SD(int a, int b) {
+	double sumSD = 0;
+	double SD;
+	double k = 0;
+	if (a > b)
+	{
+		for (int i= a; i >= b; i--)
+		{
+			sumSD += pow(i - avg(a,b), 2);
+			k++;
+		}
+		SD = sqrt(sumSD / (k-1.0));
+		return SD;
+	}
+	else if (a < b)
+	{
+		for (int j = a; j <= b; j++)
+		{
+			sumSD += pow(j - avg(a, b), 2);
+			k++;
+		}
+		SD = sqrt(sumSD / (k-1.0));
+		return SD;
+	}
+
 }
